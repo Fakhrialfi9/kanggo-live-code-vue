b07787cbaaaa036d55726b533012e15ef9b80f04
M Fakhri Alvi Syahrin <fakhrialfi9@gmail.com>
2025-07-31 16:30:27 +0700

    Test Live Code Kandidat Frontend - Update Router

--- src/main/router/index.js
+++ src/main/router/index.js
@@ -3,19 +3,9 @@ import VueRouter from "vue-router";
 import adminComponents from "@/ui/pages/admin/admin.vue";
 
 Vue.use(VueRouter);
-
-let userProfile = null;
-
 // get userProfile from localStorage
-try {
-  userProfile = JSON.parse(localStorage.getItem("userProfile"));
-} catch (e) {
-  console.error("Invalid userProfile data in localStorage", e);
-}
 
 // get user role from userProfile
-const userRole = userProfile.role;
-console.log("userRole", userRole);
 
 // Buat router dinamis berdasarkan current user role, good luck.
 
@@ -38,15 +28,5 @@ const router = new VueRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
-  const profile = JSON.parse(localStorage.getItem("userProfile"));
-  const requiredRole = to.meta.requiresRole;
-
-  if (requiredRole && profile?.role !== requiredRole) {
-    return next("/404");
-  }
-
-  next();
-});
 
 export default router;
